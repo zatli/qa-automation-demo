ee420f4820a4c028ffd05bc12b5aef1ccbef64b5
Jan Zatloukal <zatlhonza@gmail.com>
2025-10-26 21:08:59 +0100

    fix(ci): changes

--- cypress.config.js
+++ cypress.config.js
@@ -3,13 +3,16 @@ const { defineConfig } = require('cypress');
 module.exports = defineConfig({
   // Configure E2E testing
   e2e: {
+    // This defines where Cypress looks for your test files (correctly targets .ts and .js in the e2e folder)
     specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
+    // This is the base URL for tests that use cy.visit('/')
     baseUrl: 'https://example.com', 
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
-  video: false,
-  reporter: 'spec',
+
+  // General configuration settings
+  video: false, // Turn off video recording for faster CI runs
+  reporter: 'spec', // Use the clean spec reporter
 });
-module.exports = {}; 
