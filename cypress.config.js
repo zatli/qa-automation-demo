9d12a2ef88aaf1b74c392f77dfc7a4b49262758e
Jan Zatloukal <zatlhonza@gmail.com>
2025-10-26 21:11:48 +0100

    fix(config): explicitly disabled supportFile to resolve CI runner error

--- cypress.config.js
+++ cypress.config.js
@@ -7,6 +7,9 @@ module.exports = defineConfig({
     specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
     // This is the base URL for tests that use cy.visit('/')
     baseUrl: 'https://example.com', 
+    // FIX: Explicitly disable the support file to fix the CI error, 
+    // since we are not using custom commands in this simple demo.
+    supportFile: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
